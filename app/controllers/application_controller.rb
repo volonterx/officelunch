91fef7c0fe5c61c3a95d0e40aa9ca2cffd2fa4ba
x3enos <x3enos@gmail.com>
2013-08-26 00:27:56 +0300

    [^] localizate sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
 
+  before_filter :authenticate_user!
   protect_from_forgery with: :exception
   #before_filter :authenticate_member!
 
