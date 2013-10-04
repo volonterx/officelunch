c024f4e30e60780d90a2560fe388214c936d893d
volonterx <volonterx@gmail.com>
2013-10-04 17:14:10 +0300

    [^] add plpgsql

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130902164300) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "achievements", force: true do |t|
     t.integer  "user_id"
     t.string   "name"
